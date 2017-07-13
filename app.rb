97bc932b3e0d2dbbcfe7e4644eec0e2b7d692af9
Оксана <mari-mari@ukr.net>
2017-07-13 22:07:04 +0300

    Add plagins

--- app.rb
+++ app.rb
@@ -57,3 +57,18 @@ end
 get '/secure/place' do
   erb 'This is a secret place that only <%=session[:identity]%> has access to!'
 end
+
+get '/visit' do
+  erb :visit
+end
+
+post '/visit' do
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @barber = params[:barber]
+  @color = params[:color]
+
+
+  erb "<h2>Спасибо, что записались </h2>"
+end
